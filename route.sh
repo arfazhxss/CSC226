7a8f03821e3751d8673bf358be0a0b214854d4de
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-19 22:45:44 -0700

    Routine Commit by @arfazhxss on Fri 19 May 2023 22:45:44 PDT

--- route.sh
+++ route.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-# route git 3.9
+# route git 3.8
 
-rm -rf .DS_Store/
 git add . && \
 git add -u && \
 git commit -m $'Routine Commit\nby @arfazhxss on '"$(date)" && \
 git push origin HEAD
-rm -rf .DS_Store/
-rm -rf .DS_Store/
-rm -rf .DS_Store/
-rm -rf .DS_Store/
-rm -rf .DS_Store/
-rm -rf .DS_Store/
