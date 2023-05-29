cec7e46c69cca1bd9c8c91d68cdbee00292306a6
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-28 20:30:06 -0700

    Test Schedule 2 by @arfazhxss on Sun 28 May 2023 20:30:06 PDT

--- route-test.sh
+++ route-test.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# route git 4.1
 
 echo "Enter commit message:"
 read commit_message
 
-rm -rf .DS_Store/
 git add . && \
 git add -u && \
-git commit -m $"$commit_message\nby @arfazhxss on $(date)" && \
+git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date)" && \
 git push origin HEAD
-git log > commit-hist.txt
-rm -rf .DS_Store/
