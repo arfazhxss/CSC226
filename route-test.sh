68c5a5e89986b30cb595c73245c1afc5ec22663c
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-28 20:33:51 -0700

    Test Success, Routine Back 1 Scheduled Commit by @arfazhxss on Sun 28 May 2023 20:33:51 PDT

--- route-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter commit message:"
-read commit_message
-
-git add . && \
-git add -u && \
-git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date)" && \
-git push origin HEAD
