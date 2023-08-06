3f18a475251b88c9d40c5427f05e2cb199cb01f8
arfazhxss <arfazhussain@uvic.ca>
2023-08-05 22:48:41 -0700

    Routine Commit by @arfazhxss on Sat 05 Aug 2023

--- route.sh
+++ route.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#auto-git v6.2
+#auto-git v6.5
 
 find . -name ".DS_Store" -type f -delete
 echo -e ".DS_Store\ncommit-hist.txt" > .gitignore
-
-echo -e "\n_______________________________________________________\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
+unEnScore=50
+unEnString=$(printf '_%.0s' $(seq 1 "$unEnScore"))
+echo -e "\n${unEnString}\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
 read -s -n 3 -p "(yes/ENTER): " answer
 
 if [[ $answer == "yes" || $answer == "Yes" || $answer == "YES" ]]; then
-  echo -e "YES'ED\n-------------------------------------------------------"
+  echo -e "YES'ED\n${unEnString}"
   git stash
   git stash clear
-  git pull origin main
-  echo -e "-------------------------------------------------------\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n_______________________________________________________"
+  git pull
+  echo -e "${unEnString}\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n${unEnString}"
 else
-  echo -e "ENTER'ED\n-------------------------------------------------------"
+  echo -e "ENTER'ED\n${unEnString}"
   read -p "Your Commit Message: " commt
 
   if [[ -z "$commt" || ${#commt} -lt 3 ]]; then
@@ -25,7 +26,7 @@ else
     git push origin HEAD
     git log > commit-hist.txt
     rm -Rf .DS_Store/
-    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+    echo -e "${unEnString}\n\t\tYour changes has been pushed\n\t\tto the repository :)\n${unEnString}"
   else
     git add . && \
     git add -u && \
@@ -33,6 +34,6 @@ else
     git push origin HEAD
     git log > commit-hist.txt
     rm -Rf .DS_Store/
-    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+    echo -e "${unEnString}\n\t\tYour changes has been pushed\n\t\tto the repository :)\n${unEnString}"
   fi
 fi
